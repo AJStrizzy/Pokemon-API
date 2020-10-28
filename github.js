2f6f84650a6699a5e3496f036b812dc68b81f4c6
Ariel <Ariel@Maddies-MacBook-Pro.local>
2020-10-28 11:37:24 -0400

    [JS] added function to add API information into document

--- github.js
+++ github.js
@@ -1,11 +1,27 @@
-fetch("https://api.github.com/users/ajstrizzy");
+const container = document.querySelector(".container")
+
+
+fetch("https://api.github.com/users/ajstrizzy")
     .then(response => {
         return response.json();
     })
     .then(githubData => {
-        console.log(githubData);
-    });
+        const ariel = {
+            name: githubData.name,
+            bio: githubData.bio,
+            username: githubData.username,
 
+        }
+        console.log(ariel);
+
+        const newElement = document.createElement("p");
+        newElement.textContent = ariel.name;
+        container.appendChild(newElement);
+    })
+
+.catch(error => {
+    console.log(error);
+});
     
 
 
